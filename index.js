af27c2d204d656ebd4e73085932437dfa6474770
akmalsyrf <dev.akmal69@gmail.com>
2021-12-21 10:43:46 +0700

    update

--- index.js
+++ index.js
@@ -1,38 +1,78 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 //allow this app to receive incoming json request
 //Create app.use for express.json here
+app.use(express.json());
 
 let todos = [
-    {
-        id: 1,
-        title: "Cuci tangan",
-        isDone: true
-    },
-    {
-        id: 2,
-        title: "Jaga jarak",
-        isDone: false
-    },
-]
+  {
+    id: 1,
+    title: "Cuci tangan",
+    isDone: true,
+  },
+  {
+    id: 2,
+    title: "Jaga jarak",
+    isDone: false,
+  },
+];
 
 //GET list route: simply send arr of obj todos on your user screen
 // Create method GET here
+app.get("/todos", (req, res) => {
+  res.send({
+    data: todos,
+  });
+});
 
 //GET detail route: send the todo obj, by received id request params
 // Create method GET by received id here
+app.get("/todo/:id", (req, res) => {
+  const { id } = req.params;
+  const data = todos.find((item) => item.id == id);
+
+  if (!data) {
+    res.send({ status: "Failed", message: "Data not found" });
+  }
+
+  res.send({ data });
+});
 
 //POST route: receive json body request, from user input, then push to todos array
 // Create method POST here
+app.post("/todo", (req, res) => {
+  const data = req.body;
+  todos.push(data);
+
+  res.send({ todos });
+});
 
 //PATCH route: receive json body request, from user input, then push to todos array
 //by object id
 // Create method PATCH here
+app.patch("/todo/:id", (req, res) => {
+  const { id } = req.params;
+
+  todos = todos.map((item) => {
+    if (item.id == id) {
+      return { ...req.body };
+    }
+    return item;
+  });
+
+  res.send({ data: todos });
+});
 
 //DELETE route: delete the todo obj, by received id request params
 // Create method DELETE here
+app.delete("/todo/:id", (req, res) => {
+  const { id } = req.params;
+  todos = todos.filter((item) => item.id != id);
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+  res.send({ data: todos });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));

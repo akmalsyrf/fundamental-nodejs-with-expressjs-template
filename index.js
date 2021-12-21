6015313b33d3af85b5543dc26260c2bee31786ea
akmalsyrf <dev.akmal69@gmail.com>
2021-12-21 09:49:17 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //instantiate express module here
-
+const express = require("express");
 //use express in app variable here
+const app = express();
 
 //define the server port here
-
+const port = 5000;
 //create the homepage route here and inside it create res means, response, and it send string "Hello Express!" to the API
-
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 // Create listen here
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
